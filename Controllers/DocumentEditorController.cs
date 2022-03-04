c352951ad7961ce55f1a318569281142634bd1d2
Tharindu Kumarasinghe <57245404+TharinduK97@users.noreply.github.com>
2022-03-04 22:32:11 +0530

    first commit

---
+++ Controllers/DocumentEditorController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Syncfusion.EJ2.SpellChecker;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Syncfusion_document_editor.Controllers
+{
+    [Route("api/[controller]")]
+    public class DocumentEditorController : Controller
+    {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        List<DictionaryData> spellDictionary;
+        string personalDictPath;
+        string path;
+
+        public DocumentEditorController(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            spellDictionary = Startup.spellDictCollection;
+            path = Startup.path;
+            personalDictPath = Startup.personalDictPath;
+
+        }
+
+        [AcceptVerbs("Post")]
+        [HttpPost]
+       // [EnableCors("AllowAllOrigins")]
+        [Route("SpellCheck")]
+        public  string SpellCheck([FromBody] SpellCheckJsonData spellChecker)
+        {
+
+            try
+            {
+                // SpellCheckDictionary ss = new SpellCheckDictionary(spellDictionary[0]);
+                List<SpellCheckDictionary> ss = new List<SpellCheckDictionary>();
+                foreach (var spell in spellDictionary)
+                {
+                    if(spell.LanguadeID == spellChecker.LanguageID)
+                    {
+                        ss.Add(new SpellCheckDictionary(spell));
+                    }
+                    
+                }
+                SpellChecker spellCheck = new SpellChecker(ss);
+                spellCheck.GetSuggestions(spellChecker.LanguageID, spellChecker.TexttoCheck, spellChecker.CheckSpelling, spellChecker.CheckSuggestion, spellChecker.AddWord);
+                return Newtonsoft.Json.JsonConvert.SerializeObject(spellCheck);
+            }
+            catch(Exception e)
+            { 
+                return "{\"SpellCollection\":[],\"HasSpellingError\":false,\"Suggestions\":null}";
+            }
+        }
+
+
+        [AcceptVerbs("Post")]
+        [HttpPost]
+        [EnableCors("AllowAllOrigins")]
+        [Route("SpellCheckByPage")]
+        public string SpellCheckByPage([FromBody] SpellCheckJsonData spellChecker)
+        {
+            try
+            {
+                SpellChecker spellCheck = new SpellChecker(spellDictionary, personalDictPath);
+                spellCheck.CheckSpelling(spellChecker.LanguageID, spellChecker.TexttoCheck);
+                return Newtonsoft.Json.JsonConvert.SerializeObject(spellCheck);
+            }
+            catch
+            {
+                return "{\"SpellCollection\":[],\"HasSpellingError\":false,\"Suggestions\":null}";
+            }
+        }
+
+        public class SpellCheckJsonData
+        {
+            public int LanguageID { get; set; }
+            public string TexttoCheck { get; set; }
+            public bool CheckSpelling { get; set; }
+            public bool CheckSuggestion { get; set; }
+            public bool AddWord { get; set; }
+
+        }
+
+
+
+    }
+
+
+}
---
+++ Startup.cs
@@ -0,0 +1,130 @@
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Syncfusion.EJ2.SpellChecker;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Syncfusion_document_editor
+{
+    public class Startup
+    {
+        private string _contentRootPath = "";
+        internal static List<DictionaryData> spellDictCollection;
+        internal static string path;
+        internal static string personalDictPath;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                 .SetBasePath(env.ContentRootPath)
+                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                 .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+            _contentRootPath = env.ContentRootPath;
+
+            path = "";
+            string jsonFileName = "spellcheck.json";
+            //check the spell check dictionary path environment variable value and assign default data folder
+            //if it is null.
+            path = string.IsNullOrEmpty(path) ? Path.Combine(env.ContentRootPath, "App_Data") : Path.Combine(env.ContentRootPath, path);
+            //Set the default spellcheck.json file if the json filename is empty.
+        jsonFileName = string.IsNullOrEmpty(jsonFileName) ? Path.Combine(path, "spellcheck.json") : Path.Combine(path, jsonFileName);
+            if (System.IO.File.Exists(jsonFileName))
+            {
+                string jsonImport = System.IO.File.ReadAllText(jsonFileName);
+                List<DictionaryData> spellChecks = JsonConvert.DeserializeObject<List<DictionaryData>>(jsonImport);
+                spellDictCollection = new List<DictionaryData>();
+                //construct the dictionary file path using customer provided path and dictionary name
+                foreach (var spellCheck in spellChecks)
+                {
+                    spellDictCollection.Add(new DictionaryData(spellCheck.LanguadeID,
+                        Path.Combine(path, spellCheck.DictionaryPath), Path.Combine(path, spellCheck.AffixPath),
+                        Path.Combine(path, spellCheck.PersonalDictPath)));
+
+                    personalDictPath = Path.Combine(path, spellCheck.PersonalDictPath);
+                }
+            }
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Syncfusion_document_editor", Version = "v1" });
+            });
+
+            ///   services.AddOData();
+            //   services.AddMvc().AddJsonOptions(x => {
+            //       x.JsonSerializerOptions.PropertyNamingPolicy = null;
+            //   });
+            services.AddMemoryCache();
+            services.AddMvc();
+            services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                // Use the default property (Pascal) casing
+                options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                });
+            });
+          //  services.AddMemoryCache();
+          //  services.AddMvc();
+          //  services.AddControllers().AddNewtonsoftJson(options =>
+          //  {
+                // Use the default property (Pascal) casing
+           //    options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+          //  });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Syncfusion_document_editor v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors("AllowAllOrigins");
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
